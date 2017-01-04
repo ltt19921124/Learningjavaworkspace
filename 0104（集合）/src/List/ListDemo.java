42ed6ad145055304d2ec36c99c51a5fd485f8211
ltt19921124 <ltt19921124@163.com>
2017-01-05 01:19:47 +0800

    k
    k

---
+++ 0104（集合）/src/List/ListDemo.java
@@ -0,0 +1,28 @@
+package List;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+public class ListDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		List list = new ArrayList();
+//		show(list);
+		list.add("1");
+		list.add("2");
+		list.add("3");
+		list.add("4");
+		
+		System.out.println("sublist:"+ list.subList(1, 2));//всап╠М
+		
+		System.out.println(list);
+	}
+	
+//	public static void show(List list ){
+//		list.add("abc1");
+//	}
+}
