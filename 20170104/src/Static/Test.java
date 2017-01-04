282e9f821db4114de067139bbbbabb84a6394aa3
ltt19921124 <ltt19921124@163.com>
2017-01-04 18:26:17 +0800

    k
    k

--- 20170104/src/Static/Test.java
+++ 20170104/src/Static/Test.java
@@ -6,7 +6,13 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		Person p1 = new Person();
+		Person p2 = new Person();
+		
+		Person.i = 10;
+		
+		System.out.println("p1的i值为："+ p1.i);
+		System.out.println("p2的i值为：" + p2.i);
 	}
 
 }
