8cf3abcd73277199a288ce185cd7a717b5f90bdf
ltt19921124 <ltt19921124@163.com>
2017-01-21 11:11:03 +0800

    k
    k

--- 20170120/src/array/Test.java
+++ 20170120/src/array/Test.java
@@ -6,9 +6,25 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int[] arr = {89,67,80,34,56,120,908};
+		int[] arr = { 89, 67, 80, 34, 56, 120, 908 };
+		/*
+		 * 遍历数组
+		 */
+//		for (int i = 0; i < arr.length - 1; i++) {
+//			System.out.println("arr[" + i + "]" + ":" + arr[i]);
+//		}
+		
+		/*
+		 * 获取最值
+		 * */
 		
 		
 	}
 
 }
+
+
+
+
+
+
