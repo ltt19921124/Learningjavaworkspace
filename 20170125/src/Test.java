02a303c4deec75b52df415688683b79217e65fff
ltt19921124 <ltt19921124@163.com>
2017-01-25 17:29:14 +0800

    k
    k

---
+++ 20170125/src/Test.java
@@ -0,0 +1,19 @@
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/*一维数组：int []arr = new int[3];
+		 * 二维数组
+		 * 
+		 * 
+		 * */
+		int[][]arr = new int[3][2];//二维数组定义方式。
+		
+		
+		
+	}
+
+}
