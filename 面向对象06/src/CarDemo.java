66fc412510154dd03c470d6a217563d334be713d
ltt19921124 <ltt19921124@163.com>
2017-01-27 22:18:39 +0800

    面向对象
    k

---
+++ 面向对象06/src/CarDemo.java
@@ -0,0 +1,21 @@
+
+public class CarDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/*
+		 * 描述小汽车
+		 * 分析：
+		 * 	1,属性
+		 * 		轮胎数，颜色
+		 * 
+		 * 
+		 * 	2,行为
+		 * 		运行
+		 * 
+		 * */
+	}
+
+}
