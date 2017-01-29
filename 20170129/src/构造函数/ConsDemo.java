802796950d4575fe9750019266458f33c15245dc
ltt19921124 <ltt19921124@163.com>
2017-01-30 01:27:43 +0800

    k
    k

--- 20170129/src/构造函数/ConsDemo.java
+++ 20170129/src/构造函数/ConsDemo.java
@@ -7,9 +7,19 @@ class Person {
 	// 定义一个Person类的构造函数
 
 	Person() {// 空参数的构造函数
+		name = "baby";//初始化动作
+		age = 1;
 		System.out.println("person run");
 	}
-
+	//如果有的孩子一出生有名字
+	Person(String n){
+		name = n;
+	}
+	
+	Person(String n,int a){
+		name = n;
+		age = a;
+	}
 	public void speak() {
 		System.out.println(name + ":" + age);
 	}
@@ -23,6 +33,14 @@ public class ConsDemo {
 	public static void main(String[] args) {
 		Person p = new Person();//运行打印出person run 
 		//构建创造对象时调用的函数，作用：给对象初始化。
+//		p.speak();
+		Person p1 = new Person("旺财");
+		p1.speak();
+		Person p2 = new Person("小强",10);
+		p2.speak();
+		String s = "abc";
+		System.out.println(s);
 	}
+	
 
 }
