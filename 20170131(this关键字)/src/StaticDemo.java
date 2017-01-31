fedfc47dabcb266bfab0fe0663975eb7d3ace366
ltt19921124 <ltt19921124@163.com>
2017-01-31 15:20:37 +0800

    k
    k

--- 20170131(this关键字)/src/StaticDemo.java
+++ 20170131(this关键字)/src/StaticDemo.java
@@ -4,22 +4,29 @@ class Person{
 	String name;//成员变量，实例变量
 	static String country = "CN";//静态变量，类变量
 	public void show(){
-		System.out.println(country+":" + name);
+		System.out.println(Person.country+":" + name);
 	}
 }
 
 public class StaticDemo {
-
+	int num = 4;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Person p  = new Person(); 
+//		Person p  = new Person(); 
 //		p.name = "小强";
 //		p.show();//CN:小强
-		System.out.println(p.country);//CN
-		System.out.println(Person.country);//CN
+//		System.out.println(p.country);//CN
+//		System.out.println(Person.country);//CN
+		new Person().show();
 		
+	}
+	public void show(){
+		System.out.println(num);
 	}
 
 }
+
+
+
