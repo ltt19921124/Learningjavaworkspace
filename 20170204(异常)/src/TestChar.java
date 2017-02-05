c268828f8ac923de1215102f6e3ba5656838179b
ltt19921124 <ltt19921124@163.com>
2017-02-06 00:34:02 +0800

    k
    线程安全(Mars)

--- 20170204(异常)/src/TestChar.java
+++ 20170204(异常)/src/TestChar.java
@@ -13,6 +13,8 @@ public class TestChar {
 			char[] buffer = new char[100];//×Ö½ÚÁ÷
 			int temp = fr.read(buffer,0,buffer.length);
 			fw.write(buffer,0,temp);
+			String s = new String(buffer);
+			System.out.println(buffer);
 //			for(int i=0;i<buffer.length;i++){
 //				System.out.println(buffer[i]);
 //			}
