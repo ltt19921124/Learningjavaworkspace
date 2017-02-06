9c6cda67bb5c4fcc5b83b30b97b3d65e69d38052
ltt19921124 <ltt19921124@163.com>
2017-02-07 02:06:08 +0800

    k
    k

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/22/a0f3dbe796ec00161de393ff3ae5a666
@@ -0,0 +1,12 @@
+package °ü;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
--- 20170207(包)/src/包/Test.java
+++ 20170207(包)/src/包/Test.java
@@ -1,12 +1,17 @@
 package °ü;
 
+import java.util.Scanner;
+
 public class Test {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		Scanner in = new Scanner(System.in);
+		
+		int number = in.nextInt();
+		in.close();
 	}
 
 }
