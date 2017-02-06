ddd6e8b147990b373d00c9a9654ccda0fd41894f
ltt19921124 <ltt19921124@163.com>
2017-02-07 01:39:11 +0800

    k
    k

--- 20170206(Object)/src/object类/ObjectDemo.java
+++ 20170206(Object)/src/object类/ObjectDemo.java
@@ -8,8 +8,18 @@ class Person {
 	}
 	//比较的是Person年龄是否是同龄人
 	public boolean equals(Object obj){
+		if(!(obj instanceof Person)){
+//			return false;
+			throw new ClassCastException("类型错误");
+		}
 		Person p =(Person)obj;
 		return this.age == p.age;
+	}
+//	public int hashCode(){
+//		return age;
+//	}
+	public String toString(){
+		return "Person:" + age;
 	}
 }
 
@@ -23,7 +33,10 @@ public class ObjectDemo {
 		Person p2 = new Person(20);
 //		Person p3 = p1;
 //		System.out.println(p1 == p2);//false
-		System.out.println(p1.equals(p2));//false
+//		System.out.println(p1.equals(p2));//false
+		System.out.println(p1);//366712642
+		System.out.println(p1.getClass().getName()+"@"+Integer.toHexString(p1.hashCode()));
+//		System.out.println(Integer.toHexString(p2.hashCode()));//366712642
 	}
 
 }
