b2b851de92f707840dfe7861133ef8352466ca17
ltt19921124 <ltt19921124@163.com>
2017-02-08 23:39:07 +0800

    k
    StringBuffer

--- 20170208(StringBuffer)/src/StringBuffer/StringBufferDemo.java
+++ 20170208(StringBuffer)/src/StringBuffer/StringBufferDemo.java
@@ -12,18 +12,29 @@ public class StringBufferDemo {
 		 * 		特点：
 		 * 			1，长度是可变的
 		 * 			2，可以存储不同类型的数据
-		 * 			3，
+		 * 			3，最终要转成字符串进行使用
+		 * 			4,可以对字符串进行修改
 		 * 
 		 * 		既然是一个容器，应该具备哪些功能？
 		 * 			1，添加,(byte 和short类型数据不能存储)
-		 * 				append(data) 默认大小16个字符
+		 * 				StringBuffer append(data) 默认大小16个字符
 		 * 
 		 * 			2，删除		
 		 * 
 		 * 
 		 * 
 		 * */
-		bufferMethodDemo();
+//		bufferMethodDemo();
+		bufferMethodDemo_1();
+	}
+
+	private static void bufferMethodDemo_1() {
+		StringBuffer sb = new StringBuffer("abcd");
+		
+		sb.append("xixi");
+		System.out.println(sb.toString());//abcdxixi
+		sb.insert(2, "qq");//插入字符串
+		System.out.println(sb.toString());//abqqcdxixi
 	}
 
 	private static void bufferMethodDemo() {
@@ -33,9 +44,15 @@ public class StringBufferDemo {
 //		StringBuffer s1 = sb.append(4);
 //		System.out.println(sb == s1);//true
 //		System.out.println(s1);//4
-		sb.append(4);
-		sb.append(true);
+		sb.append(4).append(false);//方法调用链(尾部添加)
+//		System.out.println(sb);//4
+//		System.out.println(sb.toString());//4
+//		sb.append(true);
 		System.out.println(sb);//4true
+		sb.insert(1, "haha");
+		System.out.println(sb);//4hahafalse
+		
+		
 	}
 
 }
