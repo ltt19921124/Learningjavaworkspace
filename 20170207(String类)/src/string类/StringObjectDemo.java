cfea00e40468f7cb2a0b95c67714be2567b2fdb5
ltt19921124 <ltt19921124@163.com>
2017-02-08 20:37:56 +0800

    k
    string

--- 20170207(String类)/src/string类/StringObjectDemo.java
+++ 20170207(String类)/src/string类/StringObjectDemo.java
@@ -8,12 +8,12 @@ public class StringObjectDemo {
 	public static void main(String[] args) {
 		/*
 		 * intern()方法，可以对字符串池进行操作(规范化操作)
-		 * 
+		 * F3可以在eclipse看源码
 		 * */
 		String s1 = "abc";
 //		String s2 = "abc";
 		String s3 = new String("abc");
-		String s2 = s1.intern();
+		String s2 = s1.intern();//获取池中的数据，这时共享数据
 		System.out.println(s1==s2);//true
 		System.out.println(s1==s3);//false
 //		System.out.println(s2);//abc
