07ec3f87a1153a0bee6c775ed45e1f80b781f00d
ltt19921124 <ltt19921124@163.com>
2017-02-09 23:11:31 +0800

    k
    集合框架

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/8f/d07b8e019dee00161796bcc517c72cc3
@@ -0,0 +1,15 @@
+package WrapperDemo;
+
+public class WrapperDemo2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int num = 4;
+		
+		Integer i = 4;// i = new Integer(4) ×Ô¶¯×°Ïä	
+		
+	}
+
+}
--- 20170209/src/WrapperDemo/WrapperDemo2.java
+++ 20170209/src/WrapperDemo/WrapperDemo2.java
@@ -8,8 +8,23 @@ public class WrapperDemo2 {
 	public static void main(String[] args) {
 		int num = 4;
 		
-		Integer i = 4;// i = new Integer(4) 自动装箱	
+		num = num + 5;
 		
+		Integer i = 4;// i = new Integer(4) 自动装箱	
+		i =  i + 6;//i = i + 6;
+		
+		Integer a = new Integer(127);//JDK1.5以后，如果装箱的是一个字节，
+		//那么该数据会共享不会重新开辟空间
+		
+		Integer b = new Integer(127);
+		
+		System.out.println(a==b);//false
+		System.out.println(a.equals(b));//true
+		
+		Integer x = 129;
+		Integer y = 129;
+		System.out.println(x==y);//false
+		System.out.println(x.equals(y));//true
 	}
 
 }
