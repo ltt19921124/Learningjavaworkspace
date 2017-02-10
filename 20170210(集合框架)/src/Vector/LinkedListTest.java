e8029d21838c0d034fe2977ef4d907af6aa995ca
ltt19921124 <ltt19921124@163.com>
2017-02-10 20:02:39 +0800

    k
    Set

---
+++ 20170210(集合框架)/src/Vector/LinkedListTest.java
@@ -0,0 +1,21 @@
+package Vector;
+/*
+ * 使用linkedlist模拟一个堆栈或者队列数据结构
+ * 
+ * 堆栈：先进后出
+ * 
+ * 
+ * 队列：先进先出
+ * 
+ * 
+ * */
+public class LinkedListTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
